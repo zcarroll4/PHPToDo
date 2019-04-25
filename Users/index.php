18ff2bafdee6a47321fb6a68f8c5605fd80d7483
Zach <zcarroll@muskingum.edu>
2019-04-25 13:13:50 -0400

    update

---
+++ Users/index.php
@@ -0,0 +1,22 @@
+<?php
+require ('../header.php');
+require ('../database-info.php');
+require ('../databaseFunctions.php');
+?>
+<div class="container">
+<div class="row">
+<div class="col-lg-12 text-primary bg-dark  p-4">
+<h1>Log In Screen</h1>
+</div>
+</div>
+<div class="row mt-3">
+<div class="col text-center bg-success p-5 m-5 rounded"><a class="text-white" href="../LogIn"><h1>Log In</h1></a></div>
+<div class="col text-center bg-info p-5 m-5 rounded"><a class="text-white"  href="../Register"><h1>Register</h1></a></div>
+</div>
+
+</div>
+</div>
+<?php
+    include ('../footer.php');
+?>
+
